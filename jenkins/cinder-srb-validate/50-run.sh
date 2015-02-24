c7b3bc1135ef4a57db7dbb44a923416b82b9eb47
Jordan Pittier <JordanP@users.noreply.github.com>
2015-02-24 17:24:07 +0100

    Cleanup SRB Job: make Bashate happy

--- jenkins/cinder-srb-validate/50-run.sh
+++ jenkins/cinder-srb-validate/50-run.sh
@@ -22,7 +22,7 @@ export RE_EXEC=true
 
 export DEVSTACK_GATE_TEMPEST_REGEX=tempest.api.volume
 
-function pre_test_hook() {
+function pre_test_hook {
     local xtrace=$(set +o | grep xtrace)
     local eerror=$(set +o | grep errexit)
 
