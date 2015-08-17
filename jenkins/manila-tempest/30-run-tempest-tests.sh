d40f3f8aa1b9e5baa2829348f49a8146431010c4
Björn Schuberg <bjorn.schuberg@scality.com>
2015-08-17 16:27:53 +0200

    Manila: Configure tempest

--- jenkins/manila-tempest/30-run-tempest-tests.sh
+++ jenkins/manila-tempest/30-run-tempest-tests.sh
@@ -5,6 +5,12 @@ cp -rv /opt/stack/manila/contrib/tempest/tempest/* ${TEMPEST_DIR}/tempest/
 source devstack/inc/ini-config
 iniset ${TEMPEST_DIR}/etc/tempest.conf service_available manila True
 iniset ${TEMPEST_DIR}/etc/tempest.conf cli enabled True
+iniset ${TEMPEST_DIR}/etc/tempest.conf share multitenancy_enabled False
+iniset ${TEMPEST_DIR}/etc/tempest.conf share enable_protocols nfs
+iniset ${TEMPEST_DIR}/etc/tempest.conf share run_extend_tests False
+iniset ${TEMPEST_DIR}/etc/tempest.conf share run_shrink_tests False
+iniset ${TEMPEST_DIR}/etc/tempest.conf share run_snapshot_tests False
+iniset ${TEMPEST_DIR}/etc/tempest.conf share storage_protocol NFS
 
 cd $TEMPEST_DIR
 set +u
