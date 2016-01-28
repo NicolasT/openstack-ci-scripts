9d6fa4364508857565ff619400dbbfdbbdcd0fc0
Jordan Pittier <jordan.pittier@scality.com>
2016-01-28 16:21:01 +0100

    Manila job: Start from a blank local.conf for Devstack
    This way, we know exactly what's in there instead of relying on the
    provided sample conf file.

--- jenkins/manila-functional-tests/20-install-openstack.sh
+++ jenkins/manila-functional-tests/20-install-openstack.sh
@@ -9,13 +9,16 @@ trap "set +u && source heat-venv/bin/activate && fab destroy" ERR
 
 source /tmp/manilaci-hosts
 git clone https://github.com/openstack-dev/devstack.git
-cp devstack/samples/local.conf devstack
 
-cat >> devstack/local.conf << EOF
-disable_service horizon n-net n-novnc cinder c-api c-sch c-vol
-enable_plugin manila https://github.com/openstack/manila.git
-enable_plugin scality-manila-devstack-plugin ${SCALITY_DEVSTACK_PLUGIN}
-SCREEN_LOGDIR="\${DEST}/logs"
+cat > devstack/local.conf <<-EOF
+	[[local|localrc]]
+	DATABASE_PASSWORD=testtest; RABBIT_PASSWORD=testtest; SERVICE_TOKEN=testtest; SERVICE_PASSWORD=testtest; ADMIN_PASSWORD=testtest;
+	disable_service n-net n-xvnc n-novnc n-obj h-eng h-api h-api-cfn h-api-cw horizon cinder c-api c-sch c-vol
+	enable_service q-svc q-agt q-dhcp q-l3 q-meta
+	enable_plugin manila https://github.com/openstack/manila.git
+	enable_plugin devstack-plugin-scality ${SCALITY_DEVSTACK_PLUGIN}
+	SCREEN_LOGDIR="\${DEST}/logs"
+	USE_SCALITY_FOR_GLANCE=False
 EOF
 
 devstack/stack.sh
